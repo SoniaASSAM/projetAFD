df26abfacf84f62121ecc4e7a265e31ba16388b7
SoniaASSAM <zs.assam@gmail.com>
2018-01-19 09:02:12 +0100

    version semi finale

--- assam_haddad/ex2.py
+++ assam_haddad/ex2.py
@@ -1,22 +1,22 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 """
 Created on Mon Jan  8 22:21:46 2018
 @author: Raouf
 """
 
-
 # -*- coding: utf-8 -*-
 
+######################## EXERCICE 2 #########################
 import ex1
 import numpy as np
 import sklearn.discriminant_analysis as da
-import math
+import math as m
 from matplotlib import pyplot as plt
 from sklearn import svm
 import pandas as pds
 
-#Les paramètres
+#Global parameters 
 k = 2
 mu0 = np.array([0,0])
 mu1 = np.array([3,2])
@@ -27,46 +27,26 @@ n1 = 10
 n2 = 10
 n = n1 +n2
 
+#DataFrames
 classifApp = None
 classifTest = None
 
 
 
-    
+############ Fonction qui met à jour la moyenne ############
 def update_mu() :
     mu[0][0] = classifApp[classifApp.prediction_lda == 0].x1.mean()
     mu[0][1] = classifApp[classifApp.prediction_lda == 0].x2.mean()
     mu[1][0] = classifApp[classifApp.prediction_lda == 1].x1.mean()
     mu[1][1] = classifApp[classifApp.prediction_lda == 1].x2.mean()
-    
- 
-def naive_bayes(x) :
-    dk = []
-    for i in range(k) :
-        dk.append(delta(i,x))
-    return np.argmax(dk, axis=0)[0][0]
 
 
+############ Fonction qui met à jour pi ############
 def update_pi() :
     for i in range(len(pi)) :
         pi[i] = len(classifApp[classifApp['prediction_lda']==i]) / len(classifApp)
-       
         
-def var_LDA(x, lmbda) :
-    global sigma
-    s = sigma
-    sigma = var_sigma(lmbda)
-    z = LDA(x)
-    sigma = s
-    return z
-
- 
-def taux_bonne_classif(df) :
-    return {'skn' : len(df[df['class']==df.prediction_skn]) / len(df),
-            'lda' : len(df[df['class']==df.prediction_lda]) / len(df)}
-
-
-
+  ############ Fonction qui met à jour sigma ############
 def update_sigma() :
     
     global sigma
@@ -78,9 +58,32 @@ def update_sigma() :
     n1 = len(classifApp[classifApp.prediction_lda == 1])
     n = n0 + n1
     sigma = np.add(n0/n * sigma_0, n1/n * sigma_1)
+
+      
+############ Fonction implémentant Naive Bayes #############
+def naive_bayes(x) :
+    dk = []
+    for i in range(k) :
+        dk.append(delta(i,x))
+    return np.argmax(dk, axis=0)[0][0]
+       
+########### Fonction calculant la LDA avec paramètre lambda pour question 5 ############   
+def var_LDA(x, lmbda) :
+    global sigma
+    s = sigma
+    sigma = var_sigma(lmbda)
+    z = LDA(x)
+    sigma = s
+    return z
+
+
+########### Fonction calculant le taux de bonne classificatio ###################
+def taux_bonne_classif(df) :
+    return {'skn' : len(df[df['class']==df.prediction_skn]) / len(df),
+            'lda' : len(df[df['class']==df.prediction_lda]) / len(df)}
     
 
-
+############ Fonction implémentant l'Analyse Discriminante Linéaire ############
 def LDA(X) :
 
     global classifApp
@@ -99,12 +102,24 @@ def LDA(X) :
     return res
 
 
+######## Sigma pour question 5 ##########
 def var_sigma(lmbda) :
-    p1 = lmbda * ( n1/n * sigma + n2/n * sigma)
+    Xapp_0 = classifApp[classifApp['class'] == 0].as_matrix(["x1","x2"])
+    Xapp_1 = classifApp[classifApp['class'] == 1].as_matrix(["x1","x2"])
+    global n1
+    global n2
+    n1 = len(Xapp_0)
+    n2 = len(Xapp_1)
+    n = n1+n2
+    sigma_0 = np.cov(Xapp_0.T)
+    sigma_1 = np.cov(Xapp_1.T)
+    p1 = lmbda * ( n1/n * sigma_0 + n2/n * sigma_1)
     p2 = (1-lmbda) * np.identity(len(sigma))
     return p1+p2
 
 
+
+############ Fonction qui print la frontière de décision ############
 def print_decision_boundary(X0,X1) :
     plt.scatter(X0[:,0],X0[:,1], c='red')
     plt.scatter(X1[:,0],X1[:,1], c= 'blue')
@@ -119,29 +134,49 @@ def print_decision_boundary(X0,X1) :
     plt.plot(xx, yy, 'k-')
     plt.show()
 
- 
+############ Fonction qui calcule le delta ############
 def delta(k,x) :
     sigma_inversee = np.linalg.inv(sigma)
     uk = np.matrix(mu[k]).T
     #print(x.T*sigma_inversee*uk -1/2*mu[k]*sigma_inversee*uk)
     p1 = np.transpose(x - 0.5 * uk) * sigma_inversee * uk
     #p1 = x.T*sigma_inversee*uk -1/2*mu[k]*sigma_inversee*uk    
-    log = math.log(pi[k])
+    log = m.log(pi[k])
     
     return p1 + log
   
+############ UNITILISE : Fonction qui print la frontière de décision avec une autre méthode############
+#Fonctionne pas super bien donc utiisation d'une autre méthode
+def decision_boundary(C0, C1, sigma, mu) :
+    w = np.dot(np.linalg.inv(sigma), np.transpose(mu[0]-mu[1]))
+    b = -0.5 * (np.transpose(mu[0]-mu[1]).dot(np.linalg.inv(sigma).dot(mu[0]+mu[1]))) + m.log(pi[0]/pi[1])
 
-def decision_boundary(sigma,mu) :
-    sigma_inversee = np.linalg.inv(sigma)
-    u = [i.reshape(1,2).T for i in mu]
-    w = sigma_inversee * (u[0] - u[1])
-    b = (u[0] - u[1]).T * sigma_inversee * (u[0] + u[1]) / 2 - math.log(pi[0]/pi[1])
-    xx = [0,-b/w[0]]
-    yy = [-b/w[1],0]
-    plt.plot(xx, yy, 'r-')
+    Y0 = [y for (x, y) in C0]
+    Y1 = [y for (x, y) in C1]
+
+    y0 = min(min(Y0), min(Y1))
+    y1 = max(max(Y0), max(Y1))
+    
+    x0 = (-b-w[1]*y0) / w[0]
+    x1 = (-b-w[1]*y1) / w[0]
+
+    P1 = [x0, y0]
+    P2 = [x1, y1]
+    
+    ##Print
+    
+    X0 = [x for (x, y) in C0] # On récupère les abcisses
+    YY0 = [y for (x, y) in C0] # On récupère les ordonnées
+    plt.plot(X0, YY0, '+',  color="red")
+    
+    X1 = [x for (x, y) in C1]
+    YY1 = [y for (x, y) in C1]
+    plt.plot(X1, YY1, '+', color="blue")
+    
+    plt.plot((P1[0], P2[0]), (P1[1], P2[1]), color="black")
     plt.show()
 
-
+#DataFrames
 def init_classifApp() :
     global classifApp
     classifApp0 = pds.DataFrame()
@@ -156,7 +191,8 @@ def init_classifApp() :
     classifApp = classifApp0.append(classifApp1, ignore_index=True)
     classifApp['prediction_lda'] = [naive_bayes(i) for i in Xapp]
     classifApp['prediction_skn'] = None
-    
+
+#DataFrames
 def init_classifTest() :
     global classifTest
     classifTest0 = pds.DataFrame()
@@ -176,7 +212,7 @@ Xapp_1 = ex1.dataGenerator(mu[1],sigma,10)
 Xapp = np.concatenate((Xapp_0,Xapp_1))
 
 
-
+#Fonction applicant LDA de la librairie Sklearn
 def skn(X) :
     y = np.hstack((np.zeros(10), np.ones(10)))
     clf = da.LinearDiscriminantAnalysis()
@@ -192,49 +228,114 @@ Xtest_1 = ex1.dataGenerator(mu[1],sigma,1000)
 Xtest = np.concatenate((Xtest_0,Xtest_1))
 
 
+############ MAIN ############
 if __name__ == '__main__' :
-    #Question 1
     
-    print("Question 1 : ")
+    
+    ############## Question 1 ####################
+    print("##Question 1 : \n")
     init_classifApp()
     init_classifTest()
-    print("Variable à prédire : Données tests")
     res_impl = LDA(Xtest)
     res_skn =skn(Xtest)
     classifApp['prediction_skn'] = skn(classifApp.as_matrix(['x1','x2']))
-    print("Résultats identitques ? "
-          +str(np.array_equal(res_skn,res_impl)))
-    #Résultats différents
-    print("Taux de bonne clasification : "+
+    
+    #Print results
+    print(classifApp)
+    print("\n - Taux de bonne clasification : "+
           str(taux_bonne_classif(classifApp)))
-    #print("Frontiere décision avant")
-    #print_decision_boundary(Xapp_0,Xapp_1)
-    #Question 2
-    print("Question 2 : ")
+    
+    print("\n - Résultats identitques ? "
+          +str(np.array_equal(res_skn,res_impl))) ##Retourne booléen
+    
+    print("\n - Frontiere décision Q1")
+    decision_boundary(Xapp_0,Xapp_1, sigma, mu)
+    decision_boundary(Xtest_0,Xtest_1, sigma,mu)
+    
+    ############## Question 2 & 3 ####################
+    print("\n\n\n ##Question 2 : ")
+   
+    Xapp_0 = ex1.dataGenerator(mu[0],sigma,10)
+    Xapp_1 = ex1.dataGenerator(mu[1],sigma,10)
+    Xapp = np.concatenate((Xapp_0,Xapp_1))
     Xapp_0[0] = [-10,-10]
     Xapp = np.concatenate((Xapp_0,Xapp_1))
     init_classifApp()
+    
+    #MAJ données de test après changement de la donnée d'app
+    update_mu()
+    update_sigma()
+    pi = [1/2,1/2]
+    Xtest_0 = ex1.dataGenerator(mu[0],sigma,1000)
+    Xtest_1 = ex1.dataGenerator(mu[1],sigma,1000)
+    Xtest = np.concatenate((Xtest_0,Xtest_1))
+
     init_classifTest()
-    print("Résultats après modif des données app")
     res_impl2 = LDA(Xtest)
     res_skn2 = skn(Xtest)
     classifApp.loc[20:,['prediction_skn']] = res_skn2
-    print("Résultats identitques ? "
-          +str(np.array_equal(res_skn2,res_impl2)))
-    print("Taux de bonne clasification : "+
+    
+    #Print results
+    print("\n - Résultats après modification de la première observation de la classe C0 : ")
+    print("\n - Taux de bonne clasification : "+
           str(taux_bonne_classif(classifApp)))
-    #print(classifApp)
-    #print("frontiere apres")
-    #print_decision_boundary(Xapp_0,Xapp_1)
-    #Même résultats que précedents ?
-    #TODO Pourquoi ?
-    #Question3
-    print("Question 3 : ")
-    print("Frontière de décision: ")
+    
+    print("\n - Résultats identitques ? "
+          +str(np.array_equal(res_skn2,res_impl2)))
+     
+    print("\n - Frontière de décision Q2")
     print_decision_boundary(Xapp_0,Xapp_1)
-    #TODO Question4
+    print_decision_boundary(Xtest_0,Xtest_1)
+
+    
+    #Question4
     #Faire éloigner la cov mettre exemple 10 5 5 10 => On aura des taux de bonnes classif à 60%
+    
     #Question 5
     #Pour lambda = 1 On se retrouve avec les parametres de LDA
-    print("Question 5 :")
-    print(np.array_equal(var_LDA(Xtest,0),res_impl2))
+    
+    print("\n\n\n##Question 5 : comparaison LDA & Nouvelle formule avec lambda = 1")
+    mu0 = np.array([0,0])
+    mu1 = np.array([3,2])
+    mu = [mu0,mu1]
+    sigma = np.matrix([[1,0.5], [0.5, 1]])
+    pi = [1/2,1/2]
+    Xapp_0 = ex1.dataGenerator(mu[0],sigma,10)
+    Xapp_1 = ex1.dataGenerator(mu[1],sigma,10)
+    Xapp = np.concatenate((Xapp_0,Xapp_1))
+    Xtest_0 = ex1.dataGenerator(mu[0],sigma,1000)
+    Xtest_1 = ex1.dataGenerator(mu[1],sigma,1000)
+    Xtest = np.concatenate((Xtest_0,Xtest_1))
+    init_classifApp()
+    init_classifTest()
+    
+    print("\nTracer la courbe des taux en fonction des lambdas")
+    print("\n\t## ATTENTION ## Traitement assez lent (1 à 2m), pensez à prendre un café avec vous")
+    
+    taux = []
+    pas = 0.1
+    lmbdas = []
+    #Données d'apprentissage
+    app_0 = ex1.dataGenerator(mu[0],sigma,10)
+    app_1 = ex1.dataGenerator(mu[1],sigma,10)
+    app = np.concatenate((app_0,app_1))
+    tst_0 = ex1.dataGenerator(mu[0],sigma,1000)
+    tst_1 = ex1.dataGenerator(mu[1],sigma,1000)
+    tst = np.concatenate((tst_0,tst_1))
+    for i in range(11) :
+        print(i)
+        Xapp = app
+        Xtest = tst
+        mu0 = np.array([0,0])
+        mu1 = np.array([3,2])
+        mu = [mu0,mu1]
+        sigma = np.matrix([[1,0.5], [0.5, 1]])
+        pi = [1/2,1/2]
+        init_classifApp()
+        init_classifTest()
+        lmbdas.append(i*pas)
+        var_LDA(Xtest,i*pas)
+        taux.append(taux_bonne_classif(classifApp)['lda'])
+    plt.plot(lmbdas,taux)
+    plt.title("Taux de bonne classification en fonction de lambdas")
+    plt.show()
--- assam_haddad/ex3.py
+++ assam_haddad/ex3.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import ex1
+############## EXERCICE 3 #########################
+import ex1, ex2
 import numpy as np
 from sklearn import svm
 import matplotlib.pyplot as plt
 import pandas as pds
 import sklearn.discriminant_analysis as da
 
-#Choix de classifieurs 
+
+########## Question 1 : Choix de classifieurs ############################
 #1 - SVM : Support Machine Vector 
+#2 - Neuronal Network
 
 
+#Global parameters 
 k = 2
 mu0 = np.array([0,0])
 mu1 = np.array([3,2])
@@ -20,6 +24,7 @@ pi = [1/2,1/2]
 n1 = 10
 n2 = 10
 n = n1 +n2
+
 
 #Données d'apprentissage
 Xapp_0 = ex1.dataGenerator(mu[0],sigma,10)
@@ -34,6 +39,7 @@ Xtest = np.concatenate((Xtest_0,Xtest_1))
 
 classif = None
 
+#DataFrames 
 def init_classif(X0,X1) :
 
     global classif
@@ -48,12 +54,19 @@ def init_classif(X0,X1) :
     classif = classif0.append(classif1)
     classif = classif.sample(frac=1).reset_index(drop=True)
 
+#Fonction classification avec méthode SVM
 def testSVM(X) :
     clf = svm.SVC(kernel='linear', C = 1.0)
     clf.fit(Xapp,y)
+    w = clf.coef_[0]
+    a = -w[0] / w[1]
+    xx = np.linspace(-5, 5)
+    yy = a * xx - (clf.intercept_[0]) / w[1]
+    plt.plot(xx, yy, 'k-')
+    plt.show()
     return clf.predict(X)
     
-    
+#Fonction classification avec méthode de Réseau de Neurones  
 def test_neuronal_network(X) :
     from sklearn.neural_network import MLPClassifier
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
@@ -61,42 +74,80 @@ def test_neuronal_network(X) :
     clf.fit(Xapp, y) 
     return clf.predict(X)
 
-
+#Fonction classification avec méthode d'Analyse Linéaire Discriminante 
 def test_lda(X) :
     clf = da.LinearDiscriminantAnalysis()
     clf.fit(Xapp,y)
     return clf.predict(X)
 
+#Fonction calculant le taux de bonne classificatio 
 def taux_bonne_classif(classif) :
     n = len(classif)
     return {'lda' : len(classif[classif['class'] == classif.prediction_lda])/n,
             'svm' : len(classif[classif['class'] == classif.prediction_svm])/n,
             'neuronal_net' : len(classif[classif['class'] == classif.prediction_neuronal_net])/n}
 
-#Donnees générées
+############ Fonction qui print la frontière de décision ############
+def print_decision_boundary(X0,X1) :
+    plt.scatter(X0[:,0],X0[:,1], c='red')
+    plt.scatter(X1[:,0],X1[:,1], c= 'blue')
+    X = np.concatenate((X0,X1))
+    clf = svm.SVC(kernel = 'linear')
+    y = np.hstack((np.zeros(len(X0)), np.ones(len(X1))))
+    clf.fit(X, y)
+    w = clf.coef_[0]
+    a = -w[0] / w[1]
+    xx = np.linspace(-5, 5)
+    yy = a * xx - (clf.intercept_[0]) / w[1]
+    plt.plot(xx, yy, 'k-')
+    plt.show()
+    
+###################### QUESTION 2 : USING RANDOM DATA #####################
+    
 init_classif(Xtest_0, Xtest_1)
 y = np.hstack((np.zeros(len(Xapp_0)), np.ones(len(Xapp_1))))
 Xtest = classif.as_matrix(['x1','x2'])
 classif['prediction_svm'] = testSVM(Xtest)
+
+print_decision_boundary(Xtest_0,Xtest_1)
 classif['prediction_neuronal_net'] = test_neuronal_network(Xtest)
+
+print_decision_boundary(Xtest_0,Xtest_1)
 classif['prediction_lda'] = test_lda(Xtest)
+print_decision_boundary(Xtest_0,Xtest_1)
+
+#Print results
+print("## Résultats de classification avec données synthétiques : \n")
 print(taux_bonne_classif(classif))
-#Jeu de données choisi 
+
+###################### QUESTION 3 : USING EXTERNAL DATA #####################
+
+#Load data
 jdd = pds.DataFrame.from_csv("haberman.csv", header=None).reset_index()
 n = len(jdd)
 napp = int(0.8 * n)
 jdd = jdd.rename(columns={0:'x1', 1:"x2",2:'x3',3:'class'})
+
+#Données d'apprentissage
 Xapp = jdd.loc[:napp]#.as_matrix(['x1','x2','x3'])
 Xapp_0 = Xapp[Xapp['class'] == 2].as_matrix(['x1','x2','x3'])
 Xapp_1 = Xapp[Xapp['class'] == 1].as_matrix(['x1','x2','x3'])
+
+#Données de test
 Xtest = jdd.loc[napp:]
 Xtest_0 = Xtest[Xtest['class'] == 2].as_matrix(['x1','x2','x3'])
 Xtest_1 = Xtest[Xtest['class'] == 1].as_matrix(['x1','x2','x3'])
+
+#Application apprentissage
 y = np.hstack((np.zeros(len(Xapp_0)), np.ones(len(Xapp_1))))
 Xapp = np.concatenate((Xapp_0,Xapp_1))
 init_classif(Xtest_0,Xtest_1)
 Xtest = np.concatenate((Xtest_0,Xtest_1))
 classif['prediction_svm'] = testSVM(Xtest)
+
 classif['prediction_neuronal_net'] = test_neuronal_network(Xtest)
 classif['prediction_lda'] = test_lda(Xtest)
+
+#Print results
+print("\n\n## Résultats de classification avec données externes : \n")
 print(taux_bonne_classif(classif))
